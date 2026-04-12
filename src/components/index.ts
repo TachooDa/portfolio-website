4e39ef95cf43a28d62a3c6a857d255d9420ef9ce
Faraj007 <faraj.hafidh@gmail.com>
2026-04-12 10:30:03 +0700

    fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,7 +4,4 @@ export { AboutSection } from "./about-section";
 export { ProjectsSection } from "./projects-section";
 export { SkillsSection } from "./skills-section";
 export { ContactSection } from "./contact-section";
-// export { MainContent } from "./main-content";
-// components/index.ts — tambahkan ini
-// export { ScrollToTop } from "./scroll-top";
 export { MainContent } from "./main-content";
